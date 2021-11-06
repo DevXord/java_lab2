20284d8d89e820183a398499d95c145a55606e24
DevXord <xordii.xd@gmail.com>
2021-11-06 18:59:49 +0100

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,7 @@
-import java.util.Scanner;
 
 public class Main {
-
-    private static int[] tablica;
-
     public static void main(String[] args) {
 
-        /* przykład continue break */
-
-        /* przykład użycia continue */
-        for (int i = 0; i <= 10; i++) {
-            if (i % 3 == 0)
-                continue;
-            System.out.print(i + " ");
-        }
-
-        System.out.println("\n");
-
-        /* przykła użycia break */
-        for (int i = 0; i <= 100; i++) {
-            if (i == 22)
-                break;
-            System.out.print(i + " ");
-        }
-
-        System.out.println("\n");
-
-        /* przykła użycia return */
-        for (int i = 0; i <= 100; i++) {
-            if (i == 33)
-                return;
-            System.out.print(i + " ");
-        }
 
 
     }
