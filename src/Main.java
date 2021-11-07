30db39348647c0223d7c6c317a8008141937fdd4
DevXord <xordii.xd@gmail.com>
2021-11-07 15:37:37 +0100

    Commit ĆW_4

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,36 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
+        System.out.print("\n\nBreak");
+        for(int i=0; i<= 10; i++)
+        {
+            if(i==7)
+            {
 
+                break;}
+            System.out.printf(" %d",i);
+        }
 
+        System.out.print("\n\nContinue");
+        for(int i=0; i<= 10; i++)
+        {
+            if(i==5)
+            {
+
+                continue;
+            }
+            System.out.printf(" %d",i);
+        }
+
+        System.out.print("\n\nReturn");
+        for(int i=0; i <= 10; i++)
+        {
+            if(i==5)
+            {
+
+                return;
+            }
+            System.out.printf(" %d",i);
+        }
     }
 }
